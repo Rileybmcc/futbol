e573e0167aa20efd1300542cc31228daff328026
Astrid Hecht <silentaviary@gmail.com>
2022-08-02 23:43:57 -0600

    feat: Iteration 4 complete, create generator, add relevant files in ./site/

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'erb'
 
 require_relative './game_stats'
 require_relative './league_stats'
@@ -11,20 +12,20 @@ class StatTracker
   include LeagueStats
   include SeasonStats
   include TeamStats
-  # attr_reader :games, :teams, :game_teams
+  attr_reader :games, :teams, :game_teams, :erb_data
 
-  def initialize(locations)
+  def initialize(locations, data_arr)
     @games = TrackerForge.for(Game, locations[:games])
     @teams = TrackerForge.for(Team, locations[:teams])
     @game_teams = TrackerForge.for(GameTeam, locations[:game_teams])
+    @erb_data = data_arr
   end
 
-  def self.from_csv(locations)
-    StatTracker.new(locations)
+  def self.from_csv(locations, season_id = "18", team_id = "20132014")
+    StatTracker.new(locations,[season_id, team_id])
   end
 
   def get_binding
-    binding 
+    binding
   end
-  
 end
